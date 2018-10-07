66a3afec037d183cfd537e6f63b6bc544feac13f
Kristen Shea <kstar61@live.com>
2018-10-06 21:10:21 -0500

    whiteboarding exercise

--- starstack/Program.cs
+++ starstack/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace starstack
 {
@@ -6,7 +7,43 @@ namespace starstack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //     *
+            //    **
+            //   ***
+            //  ****
+            // *****
+            //******
+
+
+            //created a for loop, since i want there to be 1 star on the first line,
+            //i started my integer value at 1 instead of 0
+            //i want the last line to have 6 stars, so end integer value is 6
+
+            for (int i = 1; i <= 6; i++)
+            {   
+
+                //figured out two ways to do this: first using a string constructor new string('stringvalue', length)
+                //second is String.Concat(Enumerable.Repeat("stringvalue", length)
+
+
+                //string spaces = new string(' ', 6 - i);
+
+                string spaces = String.Concat(Enumerable.Repeat(" ", 6 - i));
+
+                //string stars = new string('*', i);
+
+                string stars = String.Concat(Enumerable.Repeat("*", i));
+
+                
+                //write solution to console
+                Console.WriteLine($"{spaces}{stars}");
+
+            }
+
+
+            Console.ReadLine();
+
         }
     }
 }
