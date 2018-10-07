ef19dc777f6ae0dcea9707e01efadc344d2eaabe
Kristen Shea <kstar61@live.com>
2018-10-06 20:16:09 -0500

    Add project files.

---
+++ starstack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace starstack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
